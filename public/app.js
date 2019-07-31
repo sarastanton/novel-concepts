32cf375e3941bd1b9491353f8d95b211d5949454
Sara Stanton <sara.mpa13@gmail.com>
2019-07-30 23:52:22 -0500

    name generator works

--- public/app.js
+++ public/app.js
@@ -1,9 +1,9 @@
 let name;
 let plot;
-let all_first_names;
-let female_first_names;
-let male_first_names;
-let last_names;
+let all_first_names = [];
+let female_first_names = [];
+let male_first_names = [];
+let last_names = [];
 let plot_button = document.getElementById("plot-button");
 let plot_space = document.getElementById("plot-result");
 let name_button = document.getElementById("name-button");
@@ -18,35 +18,36 @@ function fetchFirstNames() {
 function fetchLastNames() {
   return fetch('https://api.census.gov/data/2010/surname?get=NAME&RANK=1:1000')
     .then(response => response.json())
-    .then(names => all_last_names = names)
+    .then(names => last_names = names.slice(1));
 }
 
 function sortFirstNames() {
-  all_first_names.forEach(name =>
+  all_first_names.forEach(function(name) {
     if(name["sex"] === "M") {
-      male_first_names.push(name)
+      male_first_names.push(name["first_name"])
     } else if(name["sex"] === "F") {
-      female_first_names.push(name)
+      female_first_names.push(name["first_name"])
     }
-  )
+  })
 }
 
 function generateName() {
   let female_name_index = Math.floor(Math.random() * (female_first_names.length));
   let male_name_index = Math.floor(Math.random() * (male_first_names.length));
   let last_name_index = Math.floor(Math.random() * (last_names.length));
+  let radio_button =  document.querySelector('input[name="gender"]:checked');
 
   if(radio_button.value === "male") {
     first = properly_capitalize(male_first_names[male_name_index])
   } else if(radio_button.value === "female") {
     first = properly_capitalize(female_first_names[female_name_index])
   };
-  last = properly_capitalize(last_names[last_name_index]);
-  name = `${first last}`
+  last = properly_capitalize(last_names[last_name_index][0]);
+  name = first + " " + last;
 }
 
 function properly_capitalize(name) {
-  return `${name[0]}${name.slice(1).toLowerCase()}`
+  return name[0] + name.slice(1).toLowerCase()
 }
 
 function getPlot() {
@@ -91,4 +92,4 @@ async function gatherNames() {
 
 
 listeners()
-handleNames()
+gatherNames()
